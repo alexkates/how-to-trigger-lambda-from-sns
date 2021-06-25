df57d9ac52ee82a2596a31bd6fe08b067ee6d80e
Daniel Alexander Kates <alex.kates@creditly.com>
2021-06-25 08:30:40 -0400

    minor tweaks

--- lib/how-to-trigger-lambda-from-sns-stack.ts
+++ lib/how-to-trigger-lambda-from-sns-stack.ts
@@ -1,9 +1,25 @@
 import * as cdk from '@aws-cdk/core';
+import * as sns from '@aws-cdk/aws-sns';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as lambdaEventSources from '@aws-cdk/aws-lambda-event-sources';
 
 export class HowToTriggerLambdaFromSnsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const topic = new sns.Topic(this, 'OurSnsTopic', {
+      displayName: 'Our SNS Topic',
+    });
+
+    const lambdaFunction = new lambda.Function(this, 'Function', {
+      code: lambda.Code.fromAsset('src'),
+      handler: 'index.handler',
+      functionName: 'SnsMessageHandler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+    });
+
+    const eventSource = new lambdaEventSources.SnsEventSource(topic);
+
+    lambdaFunction.addEventSource(eventSource);
   }
 }
